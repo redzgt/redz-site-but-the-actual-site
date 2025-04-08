1ed360b5389ebec8856c9109baa8516031801eaf
redzz <164974345+redzgt@users.noreply.github.com>
2025-04-08 11:36:09 -0400

    Delete universe-games/misc/install directory

--- universe-games/misc/install/sw.js
+++
@@ -1,7 +0,0 @@
-<script>
-    if ("serviceWorker" in navigator) {
-        navigator.serviceWorker.register("./sw.js")
-        .then(() => console.log("Service Worker Registered"))
-        .catch(err => console.error("Service Worker Error:", err));
-    }
-</script>
